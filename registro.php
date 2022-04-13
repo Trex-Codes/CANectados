57cfe227989382c471948df340fae9bd98094265
Trex-Codes <juan.13JSG@gmail.com>
2022-04-13 17:41:48 -0500

    SOLVE FORM REGISTER with PHP - SQL

--- registro.php
+++ registro.php
@@ -12,24 +12,47 @@
     <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Patrick+Hand&family=Teko:wght@600&display=swap" rel="stylesheet">
 
     <style>
+
         section {
             height: 100%;
             padding: 0px;
 
             padding-bottom: 30px;
-            }
-
-            .botones {
-                margin-top: 15px;
-            }
         }
+
+        .botones {
+                margin-top: 15px;
+        }
+        
+        .error {
+            background: #DC5454;
+
+            font-size: 12px;
+        }
+
+        .correcto {
+            background-color: #3B923D;
+        }
+
+        .error, .correcto {
+             color: #fff;
+
+            font-weight: bold;
+            font-family: sans-serif;
+
+            padding: 10px;
+
+        }
+
+
 
     </style>
 </head>
 <body>
-     <?php 
+    <?php 
 
         // Validacion de datos o formularios
+
         if(isset($_POST['submitInfo'])) {
             $nombreusuario = $_POST['NombreUsuario'];
             $EdadUsuario = $_POST['EdadUsuario'];
@@ -39,38 +62,96 @@
 
             $Campos = array();
 
-            $datos = array($nombreusuario, $EdadUsuario, $NicknameUsuario, $passwordUsuario, $CorreoUsuario);
+            if($nombreusuario == ""){
+                    array_push($Campos, "El nombre no pude estar vacío");
+                }
 
-            for ($i=0; $i < count($datos) ; $i++) { 
-                echo $datos[$i] . "<br>";
+                if($EdadUsuario == "" ){
+                    array_push($Campos, "La edad no puede estar vacío");
+                }
+
+                if($NicknameUsuario == ""){
+                    array_push($Campos, "El correo no puede estar vacio");
+                }
+
+                if($passwordUsuario =="") {
+                    array_push($Campos, "La contraseña no puede estar vacia");
+                }
+
+                if($CorreoUsuario == "") {
+                    array_push($Campos, "El correo no puede estar vacio");
+                }
+
+
+                if (count($Campos) > 0) {
+                    for ($i=0; $i < count($Campos); $i++) { 
+                        echo "<aside class='error'>" . $Campos[$i] . "<br>";
+                        echo "</aside>";
+                    }   
+                } else {
+                    echo "<aside class='correcto'> Datos registrados correctamente </aside>";
+
+                }
+
+
+
+            // Conexion DB SQL
+            $servidor = 'localhost';
+            $user = 'root';
+            $password = '';
+            $db = 'canectados';
+
+            // Insersion de datos en tabla MYSQL
+            $conexion = new mysqli($servidor, $user, $password, $db);
+
+            if($conexion -> connect_error) {
+                die ("Conexion fallida" . " " . $conexion -> connect_error);
+            } else {
+                // echo "Conexion Exitosa" . "<br>";
+            }
+
+            // Insertar datos en tabla pruebaslibros db
+            $sql = "INSERT INTO usuarios (idUsuarios, nombreUsuario, edadUsuario, passwordUsuario, nicknameUsuario, correoUsuario)
+                                VALUES (NULL, '$nombreusuario', '$EdadUsuario', '$passwordUsuario', '$NicknameUsuario', '$CorreoUsuario')";
+
+            if ($conexion -> query($sql) === true) {
+                // echo "Datos insertados correctamente ";
+            } else {
+                die ("Error al insertar datos, verifique " . " " . $conexion -> error);
             }
 
 
 
-        }
+
+
+
+
+    }
      ?>
+
     <header id="Encabezado">
         <h7 id="titulo">CANectados</h7>
     </header>
     <section>
         <h1 id="bienvenida">CANectados</h1>
-        <article>
+        <article class="content">
+
             <form action="registro.php" method="post">
 
                 <label for="Nombre">Nombre: </label>
-                <input class="formulario" type="text" id="Nombre" name="NombreUsuario" placeholder="Nombre" required="">
+                <input class="formulario" type="text" id="Nombre" name="NombreUsuario" placeholder="Nombre" >
 
                 <label for="Edad">Edad: </label>
-                <input class="formulario" type="number" id="Edad" name="EdadUsuario" placeholder="Edad" min="0" max="100" required="">
+                <input class="formulario" type="number" id="Edad" name="EdadUsuario" placeholder="Edad" min="0" max="100" >
 
                 <label for="Usuario">Nickname: </label>
-                <input class="formulario" type="text" id="Usuario" name="NicknameUsuario" placeholder="Nombre de Usuario" required="">
+                <input class="formulario" type="text" id="Usuario" name="NicknameUsuario" placeholder="Nombre de Usuario" >
 
                 <label for="Contraseña">Contraseña: </label>
-                <input class="formulario" type="password" id="password" name="passwordUsuario" placeholder="Contraseña" required="">
+                <input class="formulario" type="password" id="password" name="passwordUsuario" placeholder="Contraseña" >
 
                 <label for="Correo">Correo: </label>
-                <input class="formulario" type="email" id="Correo" name="CorreoUsuario" placeholder="Correo electronico" required="">
+                <input class="formulario" type="email" id="Correo" name="CorreoUsuario" placeholder="Correo electronico" >
 
                 <input type="submit" class="botones" name="submitInfo" value="registrarse">
             </form>
@@ -81,3 +162,4 @@
     </footer>  
    </body>
 </html>
+ 
